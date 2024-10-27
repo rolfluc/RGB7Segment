9b584e3e50cb6929a4495faea90b16e7e6080e0d
Rolfes <lucasatarc@gmail.com>
2024-10-26 22:43:27 -0500

    WIP Segment Interface

---
+++ TestSoftware/SegmentDriver/SegmentDriver/SegmentInterface.h
@@ -0,0 +1,29 @@
+#pragma once
+
+typedef enum
+{
+	Display_0 = 0,
+	Display_1,
+	Display_2,
+	Display_3,
+	Display_4,
+	Display_5,
+	Display_6,
+	Display_7,
+	Display_8,
+	Display_9,
+	Display_A,
+	Display_b,
+	Display_C,
+	Display_d,
+	Display_E,
+	Display_F,
+	Display_H,
+	Display_L,
+	Display_O,
+	Display_P,
+	Display_S,
+	Display_Y,
+	Display_Nothing,
+}DisplayStates;
+
