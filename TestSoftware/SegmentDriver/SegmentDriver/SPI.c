e63149f2f7f3d36d757b7f994afe99c26d8176e8
Rolfes <lucasatarc@gmail.com>
2024-10-26 19:53:38 -0500

    Adding Untested SPI DMA Driver

---
+++ TestSoftware/SegmentDriver/SegmentDriver/SPI.c
@@ -0,0 +1,71 @@
+#include "SPI.h"
+static bool _dmaRunning = false;
+
+// TODO DMA CB set false above
+SPI_HandleTypeDef spi1;
+DMA_HandleTypeDef spi1dma;
+DMA_Channel_TypeDef sp1dmachannel;
+
+void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef * hspi)
+{
+	_dmaRunning = false;
+}
+
+static void _InitSPIPins()
+{
+	GPIO_InitTypeDef txPin;
+	txPin.Pin = GPIO_PIN_7;
+	txPin.Mode = GPIO_MODE_AF_PP;
+	txPin.Pull = GPIO_NOPULL;
+	txPin.Alternate = GPIO_AF0_SPI1; // TODO confirm
+	txPin.Speed = GPIO_SPEED_FREQ_MEDIUM; // TODO 
+	HAL_GPIO_Init(GPIOA, &txPin);
+}
+
+static void _InitSPIDMA()
+{
+	__DMA1_CLK_ENABLE();
+	spi1dma.ChannelIndex = 0; // TODO
+	spi1dma.Init.Direction = DMA_MEMORY_TO_PERIPH;
+	spi1dma.Init.PeriphInc = DMA_PINC_DISABLE;
+	spi1dma.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+	spi1dma.Init.Mode = DMA_NORMAL;
+	spi1dma.Init.Priority = DMA_PRIORITY_MEDIUM;
+	spi1dma.Init.MemInc = DMA_MINC_ENABLE;
+	spi1dma.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+	HAL_DMA_Init(&spi1dma);
+	__HAL_LINKDMA(&spi1, hdmatx, spi1dma);
+}
+
+void InitSPI()
+{
+	_InitSPIPins();
+	__SPI1_CLK_ENABLE();
+	_InitSPIDMA();
+	spi1.Instance = SPI1;
+	spi1.Init.Mode = SPI_MODE_MASTER;
+	spi1.Init.Direction = SPI_DIRECTION_1LINE;
+	spi1.Init.DataSize = SPI_DATASIZE_8BIT;
+	spi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+	spi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+	spi1.Init.NSS = SPI_NSS_SOFT;
+	spi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	spi1.Init.TIMode = SPI_TIMODE_DISABLE;
+	spi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+	spi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8; // TODO this matters a lot here.
+	HAL_SPI_Init(&spi1);
+	HAL_NVIC_SetPriority(SPI1_IRQn, 2, 0);
+	HAL_NVIC_EnableIRQ(SPI1_IRQn);
+}
+
+void SendSPIDMA(uint8_t* data, uint8_t bytes)
+{
+	HAL_SPI_Transmit_DMA(&spi1, data, bytes);
+	_dmaRunning = true;
+}
+
+// Returns true when dma Is not running.
+bool isDMAFinished()
+{
+	return !_dmaRunning;
+}
--- TestSoftware/SegmentDriver/SegmentDriver/main.c
+++ TestSoftware/SegmentDriver/SegmentDriver/main.c
@@ -14,21 +14,10 @@ int main(void)
 {
 	HAL_Init();
 
-	__GPIOC_CLK_ENABLE();
-	GPIO_InitTypeDef GPIO_InitStructure;
-
-	GPIO_InitStructure.Pin = GPIO_PIN_12;
-
-	GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStructure.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
+	__GPIOA_CLK_ENABLE();
 
 	for (;;)
 	{
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_SET);
-		HAL_Delay(500);
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_RESET);
 		HAL_Delay(500);
 	}
 }
